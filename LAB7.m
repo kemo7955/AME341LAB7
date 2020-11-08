f4f4337d4aac96e195367b18eb2d4069c5537bd2
kemo7955 <kemo7955@users.noreply.github.com>
2020-11-08 16:27:10 -0700

    hello world

--- LAB7.m
+++ LAB7.m
@@ -15,9 +15,103 @@ part3time = importdata('Data Lab 7/Part3_timeDomain.txt').data;
 sinwave777 = importdata('Data Lab 7/sinwave_777Hz.txt').data;
 triangle33 = importdata('Data Lab 7/triangle_33Hz.txt').data;
 
-%% idk anymore
+%% Figures From Part 1
+% --------------- Part 1 Sine Wave ------------------------
 figure()
-hold on; grid on
-plot(sinwave777(:,1),sinwave777(:,2),'--k')
-plot(triangle33(:,1),triangle33(:,2),'--k')
-plot(part32000(:,1),part32000(:,2),'-b','LineWidth',1.25)
+hold on; 
+set(gca,'defaulttextinterpreter','latex','TickLabelInterpreter','latex');
+plot(part1sin(:,1),part1sin(:,2),'k')
+xlabel('$f$ [Hz]');
+ylabel('Power');
+
+% --------------- Part 1 Square Wave ----------------------
+figure()
+hold on;
+set(gca,'defaulttextinterpreter','latex','TickLabelInterpreter','latex');
+plot(part1square(:,1),part1square(:,2),'k')
+xlabel('$f$ [Hz]');
+ylabel('Power');
+
+%% Figures From Part 2
+% ---------------- Sine Wave at 10kHz Sampling Rate -------
+figure()
+hold on; 
+set(gca,'defaulttextinterpreter','latex','TickLabelInterpreter','latex');
+plot(part2sin(:,1),part2sin(:,2),'k')
+xlabel('$f$ [Hz]');
+ylabel('Power');
+
+% ---------- Sine Wave at 2.5kHz Sampling Rate ------------
+figure()
+hold on;
+set(gca,'defaulttextinterpreter','latex','TickLabelInterpreter','latex');
+plot(part2sinAliasing(:,1),part2sinAliasing(:,2),'k')
+xlabel('$f$ [Hz]');
+ylabel('Power');
+
+% ---------------- Sine Wave time domain initial -------
+figure()
+hold on; 
+set(gca,'defaulttextinterpreter','latex','TickLabelInterpreter','latex');
+plot(part2Initial(:,1),part2Initial(:,2),'k')
+xlabel('t [s]');
+ylabel('V');
+
+% ---------------- Sine Wave time domain final ------------
+figure()
+hold on; 
+set(gca,'defaulttextinterpreter','latex','TickLabelInterpreter','latex');
+plot(part2Final(:,1),part2Final(:,2),'k')
+xlabel('t [s]');
+ylabel('V');
+
+% ----- Sine wave in time domain with increase Freq -------
+figure()
+hold on; 
+set(gca,'defaulttextinterpreter','latex','TickLabelInterpreter','latex');
+plot(part2Freq(:,1),part2Freq(:,2),'k')
+xlabel('t [s]');
+ylabel('V');
+
+%% Figures for part 3
+% -------------- Mystery Signal in time domain ------------
+figure()
+hold on; 
+set(gca,'defaulttextinterpreter','latex','TickLabelInterpreter','latex');
+plot(part3time(:,1),part3time(:,2),'k')
+xlabel('t [s]');
+ylabel('V');
+
+% -------------- Mystery Signal f_s at 2000 Hz ------------
+figure()
+hold on; 
+set(gca,'defaulttextinterpreter','latex','TickLabelInterpreter','latex');
+plot(part32000(:,1),part32000(:,2),'k')
+xlabel('$f$ [s]');
+ylabel('Power');
+
+% -------------- Mystery Signal f_s at 1000 Hz ------------
+figure()
+hold on; 
+set(gca,'defaulttextinterpreter','latex','TickLabelInterpreter','latex');
+plot(part31000(:,1),part31000(:,2),'k')
+xlabel('$f$ [s]');
+ylabel('Power');
+
+%% The Extra Graphs
+figure()
+
+subplot(2,1,1)
+hold on;
+set(gca,'defaulttextinterpreter','latex','TickLabelInterpreter','latex');
+plot(sinwave777(:,1),sinwave777(:,2),'k')
+plot(triangle33(:,1),triangle33(:,2),'k')
+xlabel('$f$ [s]');
+ylabel('Power');
+
+subplot(2,1,2)
+hold on;
+set(gca,'defaulttextinterpreter','latex','TickLabelInterpreter','latex');
+plot(part32000(:,1),part32000(:,2))
+xlabel('$f$ [s]');
+ylabel('Power');
